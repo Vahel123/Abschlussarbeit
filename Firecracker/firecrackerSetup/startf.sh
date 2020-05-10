e58b22f4130bda42d3f76645f0e01ac6ae8cbfd5
Vahel Hassan <Vahel.Hassan@outlook.de>
2020-05-10 09:50:00 +0200

    Update

--- Firecracker/firecrackerSetup/startf.sh
+++ Firecracker/firecrackerSetup/startf.sh
@@ -3,4 +3,5 @@
   --root-drive=hello-rootfs.ext4 \
   --kernel-opts="console=ttyS0 noapic reboot=k panic=1 pci=off nomodules rw" \
   --add-drive=file.qcow2:rw \
-  --tap-device=tap0/6e:cb:ec:da:c4:0f
+  --tap-device=tap0/ip a | grep -A1 tap0 | grep ..:[0-9\|A-Z].:..:..:..:..
+
--- Firecracker/startf.sh
+++
@@ -1,6 +0,0 @@
-./firectl \
-  --kernel=hello-vmlinux.bin \
-  --root-drive=hello-rootfs.ext4 \
-  --kernel-opts="console=ttyS0 noapic reboot=k panic=1 pci=off nomodules rw" \
-  --add-drive=file.qcow2:rw \
-  --tap-device=tap0/3a:26:c4:12:0d:11
