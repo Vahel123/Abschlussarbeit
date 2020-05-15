85ab3ad1978e539cf4df30b3e29df33e1c593d30
Vahel Hassan <Vahel.Hassan@outlook.de>
2020-05-15 19:14:31 +0200

    Update

--- Firecracker/firecrackerSetup/startf.sh
+++ Firecracker/firecrackerSetup/startf.sh
@@ -1,7 +1,9 @@
+# Die MAX Adresse muss immer an tab0 MAC Adresse angepasst werden
 ./firectl \
   --kernel=hello-vmlinux.bin \
   --root-drive=hello-rootfs.ext4 \
   --kernel-opts="console=ttyS0 noapic reboot=k panic=1 pci=off nomodules rw" \
   --add-drive=file.qcow2:rw \
-  --tap-device=tap0/ip a | grep -A1 tap0 | grep ..:[0-9\|A-Z].:..:..:..:..
+  --tap-device=tap0/72:66:9b:2e:45:c7
+
 
