e03abdf2581d4e08e0d4aac9cc09e70210997a9b
Vahel Hassan <Vahel.Hassan@outlook.de>
2020-05-18 18:55:13 +0200

    Add: start.sh for start Kata-Container

--- Firecracker/firecrackerSetup/startf.sh
+++ Firecracker/firecrackerSetup/startf.sh
@@ -4,6 +4,6 @@
   --root-drive=hello-rootfs.ext4 \
   --kernel-opts="console=ttyS0 noapic reboot=k panic=1 pci=off nomodules rw" \
   --add-drive=file.qcow2:rw \
-  --tap-device=tap0/72:66:9b:2e:45:c7
+  --tap-device=tap0/c2:6a:60:dd:6f:4f
 
 
---
+++ KataContainer/start.sh
@@ -0,0 +1,1 @@
+sudo docker run -ti --runtime kata-runtime busybox sh
