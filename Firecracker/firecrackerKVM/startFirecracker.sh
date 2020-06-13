61c68637bdb34e0a3da94ae887c5c57166b30cc9
Vahel Hassan <Vahel.Hassan123@gmail.com>
2020-06-13 23:35:28 +0200

    Update: Firecracker KVM erledgt und läuft. Firecracker-Container erledigt und läuft. Kata-Container erledigt und läuft. Docker-Container erledigt und läuft.
    Firecracker BS (Alpine) erledigt.
    Docker-Container (Alpine) erledigt.
    Kata-Container (Alpine) erledigt.
    Firecracker-Container (Debain) muss noch in Alpine abgeändert werden.

---
+++ Firecracker/firecrackerKVM/startFirecracker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -f /tmp/firecracker.socket
+
+firecracker \
+	--api-sock /tmp/firecracker.socket \
--- Firecracker/startFirecracker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f /tmp/firecracker.socket
-
-firecracker \
-	--api-sock /tmp/firecracker.socket \
