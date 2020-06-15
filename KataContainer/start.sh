6303bcf11deae0b433fdc5fd24debbf608055e23
Vahel Hassan <Vahel.Hassan123@gmail.com>
2020-06-15 19:49:29 +0200

    Update

--- KataContainer/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -rf /root/go/src/github.com/kata-containers/osbuilder/rootfs-builder/rootfs /tmp/bundle/
-bundle="/tmp/bundle"
-rootfs="$bundle/rootfs"
-mkdir -p "$rootfs" && (cd "$bundle" && kata-runtime spec)
-sudo docker export $(sudo docker create busybox) | tar -C "$rootfs" -xvf -
-sudo kata-runtime --log=/dev/stdout run --bundle "$bundle" foo
