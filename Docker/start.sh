83f7185914edf667e976a733892431c64002e790
Vahel Hassan <Vahel.Hassan123@gmail.com>
2020-07-11 22:45:17 +0200

    Update: Dockerfile - Benchmark install

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run -it vahelhassan/debian-test /bin/sh
+docker run -it -d -p 80:80 vahelhassan/debian-test sh
+
+# Nachdem der Container im background läuft können wir mit dem Befehl Docker exec innerhalb des Containers Commander ausführen
+# Syntax: docker exec -it debian-test service apache2 start
+# Benchmark
+# docker exec -it ea5cd7ab9b70 sysbench --test=cpu --cpu-max-prime=20000 run
